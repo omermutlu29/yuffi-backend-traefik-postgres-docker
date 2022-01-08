fde4a98225169506813a02e6897106093b200e2d
omermutlu29 <omermutlu29@gmail.com>
2022-01-08 23:19:59 +0300

    Treafik active

--- laravel/app/Models/BabySitter.php
+++ laravel/app/Models/BabySitter.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 class BabySitter extends Authenticatable
 {
