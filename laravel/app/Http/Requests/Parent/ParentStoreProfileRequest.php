0a689a054d9a980a5ae98d43ab3428851ac272dd
omermutlu29 <omermutlu29@gmail.com>
2022-02-13 19:06:04 +0300

    when babysitter could not add information throwing error

--- laravel/app/Http/Requests/Parent/ParentStoreProfileRequest.php
+++ laravel/app/Http/Requests/Parent/ParentStoreProfileRequest.php
@@ -34,7 +34,7 @@ class ParentStoreProfileRequest extends BaseApiRequest
             'birthday' => 'required|date_format:d-m-Y|before_or_equal:' . $date,
             //'service_contract' => 'required',
             'gender_id' => 'required|numeric|exists:genders,id',
-            'photo' => 'image|mimes:jpg,png,jpeg|max:2048',
+            'photo' => 'required|image|mimes:jpg,png,jpeg|max:2048',
             'email' => 'required|email',
         ];
     }
