cd00c2cd23fac89e70c25ba302b3fc80d0f7fb98
omermutlu29 <omermutlu29@gmail.com>
2022-02-19 22:36:09 +0300

    BabySitter IBAN Bugfix

--- laravel/app/Http/Requests/BabySitter/BabySitterUpdateGeneralInformationRequest.php
+++ laravel/app/Http/Requests/BabySitter/BabySitterUpdateGeneralInformationRequest.php
@@ -35,7 +35,7 @@ class BabySitterUpdateGeneralInformationRequest extends BaseApiRequest
             // 'birthday' => 'required|date_format:d-m-Y|before_or_equal:' . $date,
             // 'criminal_record' => 'required|file|mimes:pdf|max:2048',
             // 'address' => '',
-            'email' => 'required|unique:baby_sitters,email'.auth()->id(),
+            'email' => 'required|unique:baby_sitters,email,'.auth()->id(),
             'photo' => 'image|mimes:jpg,png,jpeg|max:2048',
             'iban' => ['required', new IBANRule()]
         ];
