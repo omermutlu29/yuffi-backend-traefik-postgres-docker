fce4eb4ffbcdf04915ed8838ca0df4e914d995f6
omermutlu29 <omermutlu29@gmail.com>
2022-03-02 17:50:24 +0300

    is_active field added to towns

--- laravel/app/Http/Requests/BabySitter/BabySitterUpdateGeneralInformationRequest.php
+++ laravel/app/Http/Requests/BabySitter/BabySitterUpdateGeneralInformationRequest.php
@@ -36,7 +36,7 @@ class BabySitterUpdateGeneralInformationRequest extends BaseApiRequest
             // 'criminal_record' => 'required|file|mimes:pdf|max:2048',
             // 'address' => '',
             'email' => 'required|unique:baby_sitters,email,'.auth()->id(),
-            'photo' => 'image|mimes:jpg,png,jpeg|max:2048',
+            'photo' => 'image|mimes:jpg,png,jpeg',
             'iban' => ['required', new IBANRule()],
             'introducing'=>'min:80|max:500'
         ];
