615323aadd47719e3d2cc0ddffdc2bcf025665be
omermutlu29 <omermutlu29@gmail.com>
2022-03-03 21:30:02 +0300

    Child name attribute added

--- laravel/database/migrations/2000_02_21_074044_create_genders_table.php
+++ laravel/database/migrations/2000_02_21_074044_create_genders_table.php
@@ -16,6 +16,7 @@ class CreateGendersTable extends Migration
         Schema::create('genders', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('child_name');
             $table->timestamps();
             $table->softDeletes();
         });
