66dfdb61897bd566f61b75e0b00e7871319b6f04
omermutlu29 <omermutlu29@gmail.com>
2022-03-08 17:58:56 +0300

    AppointmentRepository updated

--- laravel/routes/api/parent.php
+++ laravel/routes/api/parent.php
@@ -23,8 +23,8 @@ Route::prefix('parent')->group(function () {
 
 
     Route::prefix('appointment')->group(function () {
-        Route::get('get/future', [\App\Http\Controllers\API\Parent\Appointment\AppointmentController::class, 'index']);
-        Route::get('get/past', [\App\Http\Controllers\API\Parent\Appointment\AppointmentController::class, 'index']);
+        Route::get('get/future', [\App\Http\Controllers\API\Parent\Appointment\AppointmentController::class, 'getFutureAppointments']);
+        Route::get('get/past', [\App\Http\Controllers\API\Parent\Appointment\AppointmentController::class, 'getPastAppointments']);
         Route::post('create-appointment', [\App\Http\Controllers\API\Parent\Appointment\AppointmentController::class, 'createAppointment']);
         Route::post('{appointment}/pay-with-threeD', [\App\Http\Controllers\API\Parent\Appointment\AppointmentController::class, 'confirmAppointmentPayThreeD']);
         Route::post('{appointment}/pay', [\App\Http\Controllers\API\Parent\Appointment\AppointmentController::class, 'confirmAppointmentAndPay']);
