f13a7e74bef47901916f05503f45da45470ffbeb
omermutlu29 <omermutlu29@gmail.com>
2022-03-10 21:02:49 +0300

    Profile changes

--- laravel/routes/channels.php
+++ laravel/routes/channels.php
@@ -12,12 +12,12 @@ use Illuminate\Support\Facades\Broadcast;
 | used to check if an authenticated user can listen to the channel.
 |
 */
+Broadcast::routes(['middleware' => ['api']]);
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int)$user->id === (int)$id;
 });
-
-Broadcast::channel('App.Models.Appointment.{id}', function ($user,$id) {
+Broadcast::channel('App.Models.Appointment.{id}', function ($user, $id) {
     $appointment = \App\Models\Appointment::find($id);
     if (!$appointment) {
         return false;
