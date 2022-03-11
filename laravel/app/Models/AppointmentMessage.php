156f8f25826fbb3003cdacba0c0297f83e70508b
omermutlu29 <omermutlu29@gmail.com>
2022-03-11 20:49:43 +0300

    Chat Broadcast Fire

--- laravel/app/Models/AppointmentMessage.php
+++ laravel/app/Models/AppointmentMessage.php
@@ -3,14 +3,13 @@
 namespace App\Models;
 
 
-use App\Http\Resources\ChatUserResource;
 use Illuminate\Database\Eloquent\Model;
 
 class AppointmentMessage extends Model
 {
     protected $guarded = [];
 
-    protected $with = ['user'];
+    protected $with = ['userable'];
 
     public static function boot()
     {
@@ -18,7 +17,7 @@ class AppointmentMessage extends Model
         static::created(function ($appointmentMessage) {
             event(
                 new \App\Events\NewAppointmentMessageEvent(
-                    $appointmentMessage->user,
+                    $appointmentMessage->userable,
                     $appointmentMessage->message,
                     $appointmentMessage->appointment
                 )
@@ -31,7 +30,7 @@ class AppointmentMessage extends Model
         return $this->belongsTo(Appointment::class);
     }
 
-    public function user()
+    public function userable()
     {
         return $this->morphTo();
     }
