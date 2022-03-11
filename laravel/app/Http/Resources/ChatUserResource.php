03b6e89e7a06db98a56eb384c02bfef7867bce35
omermutlu29 <omermutlu29@gmail.com>
2022-03-11 20:52:04 +0300

    Chat Broadcast Fire

--- laravel/app/Http/Resources/ChatUserResource.php
+++ laravel/app/Http/Resources/ChatUserResource.php
@@ -9,6 +9,7 @@ class ChatUserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            '_id'=>$this->id,
             'full_name' => $this->name.' '.$this->last_name,
             'photo'=>$this->photo,
         ];
