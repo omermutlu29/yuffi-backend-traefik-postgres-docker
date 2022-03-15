8a5b12be69ad89027c5f153fd4180c8499613741
omermutlu29 <omermutlu29@gmail.com>
2022-03-15 22:15:32 +0300

    has booked day for next fifteen days

--- laravel/app/Services/LoginService/LoginService.php
+++ laravel/app/Services/LoginService/LoginService.php
@@ -43,7 +43,6 @@ class LoginService implements ILoginService
         $return = ['status' => false];
         try {
             $user = $userRepository->getUserByPhone($data['phone']);
-            dd($user);
             if (!$user) {
                 throw new \Exception('User could not find!',400);
             }
@@ -54,6 +53,7 @@ class LoginService implements ILoginService
             $return['token'] = $user->createToken('user')->accessToken;
             $return['user'] = $user;
             $userRepository->update($user->id,['google_st'=>$data['google_st']]);
+            dd($return);
             return $return;
         } catch (\Exception $exception) {
             throw $exception;
