6d32c4f0ee5bb9806cb5265dafca81df3c353f7a
omermutlu29 <omermutlu29@gmail.com>
2022-03-29 22:29:13 +0300

    Calendar button dynamic

--- laravel/app/Http/Controllers/API/Parent/Favorite/FavoriteController.php
+++ laravel/app/Http/Controllers/API/Parent/Favorite/FavoriteController.php
@@ -6,7 +6,6 @@ namespace App\Http\Controllers\API\Parent\Favorite;
 
 use App\Http\Controllers\API\BaseController;
 use App\Http\Requests\Parent\BabySitter\FavoriteRequest;
-use Illuminate\Support\Facades\Log;
 
 class FavoriteController extends BaseController
 {
@@ -17,29 +16,21 @@ class FavoriteController extends BaseController
 
     public function addToFavorites(FavoriteRequest $request)
     {
-        try {
-            $result = auth()->user()->favroite_baby_sitters()->attach([$request->baby_sitter_id]);
-            if (!$result) {
-                throw new \Exception('Eklenemedi', 400);
-            }
-            return $this->sendResponse(auth()->user()->favorite_baby_sitters, 'Bakıcı favroilere eklendi!');
-        } catch (\Exception $exception) {
-            return $this->sendError('Hata', ['Bir sorun ile karşılaşıldı'], 400);
+        $result = auth()->user()->favroite_baby_sitters()->attach([$request->baby_sitter_id]);
+        if (!$result) {
+            throw new \Exception('Eklenemedi', 400);
         }
+        return $this->sendResponse(auth()->user()->favorite_baby_sitters, 'Bakıcı favroilere eklendi!');
     }
 
     public function deleteFromFavorites(FavoriteRequest $request)
     {
-        try {
-            $result = auth()->user()->favroite_baby_sitters()->detach([$request->baby_sitter_id]);
-            if (!$result) {
-                throw new \Exception('Silinemedi', 400);
-            }
-            return $this->sendResponse(auth()->user()->favorite_baby_sitters, 'Bakıcı favorilerden silindi!');
-        } catch (\Exception $exception) {
-            Log::info($exception);
-            return $this->sendError('Hata', ['Bir sorun ile karşılaşıldı'], 400);
+        $result = auth()->user()->favroite_baby_sitters()->detach([$request->baby_sitter_id]);
+        if (!$result) {
+            throw new \Exception('Silinemedi', 400);
         }
+        return $this->sendResponse(auth()->user()->favorite_baby_sitters, 'Bakıcı favorilerden silindi!');
+
     }
 
 }
