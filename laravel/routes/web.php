89782ebac6bed483658a2d9c659e8402ec599e8c
omermutlu29 <omermutlu29@gmail.com>
2022-04-03 20:17:03 +0300

    Broadcast bugfix

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,7 @@ Route::get('/',function (){
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Route::any('broadcasting/auth',function (\Illuminate\Http\Request $request){
+   dd($request->all());
+});
