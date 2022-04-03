4402662609636add592f4654c25467169b3922e6
omermutlu29 <omermutlu29@gmail.com>
2022-04-03 19:00:53 +0300

    Babysitter Detail

--- laravel/app/Http/Controllers/API/Parent/Appointment/AppointmentController.php
+++ laravel/app/Http/Controllers/API/Parent/Appointment/AppointmentController.php
@@ -60,6 +60,7 @@ class AppointmentController extends BaseController
 
     public function createAppointment(CreateAppointmentRequest $request, IAppointmentService $appointmentService, BabySitterFilterService $appointmentFilterService)
     {
+        try {
             $data = $request->only('create_params');
             $data = $data['create_params'];
             $data['date'] = Carbon::createFromFormat('d-m-Y', $data['date']);
@@ -71,6 +72,9 @@ class AppointmentController extends BaseController
             } else {
                 return $this->sendError('Hata!', 'Bakıcı belirttiğiniz zaman(lar) içerisinde müsait görünmemektedir!');
             }
+        } catch (\Exception $exception) {
+            return $this->sendError('Hata', $exception->getMessage(), 400);
+        }
     }
 
     public function cancelAppointment(AppointmentCancelRequest $request, IAppointmentService $appointmentService)
