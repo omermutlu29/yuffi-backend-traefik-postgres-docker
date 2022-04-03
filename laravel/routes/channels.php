2b20629a46bf614795a1694ddd54477d5d05dd34
omermutlu29 <omermutlu29@gmail.com>
2022-04-03 22:10:40 +0300

    Broadcast bugfix

--- laravel/routes/channels.php
+++ laravel/routes/channels.php
@@ -6,7 +6,8 @@ Broadcast::channel('App.Models.Parent.{id}', function ($user, $id) {
     $parent = \App\Models\Parents::find($user->id);
     return (int)$parent->id === (int)$id;
 });
-Broadcast::channel('App.Models.BabySitter.{id}', function (\App\Models\BabySitter $user, $id) {
-    return (int)$user->id === (int)$id;
+Broadcast::channel('App.Models.BabySitter.{id}', function ($user, $id) {
+    $babySitter = \App\Models\BabySitter::find($user->id);
+    return (int)$babySitter->id === (int)$id;
 });
 
