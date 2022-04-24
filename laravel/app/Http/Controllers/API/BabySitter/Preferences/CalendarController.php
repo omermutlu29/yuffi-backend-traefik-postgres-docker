a05b94c9ecbff5a35a171f18ae3fb3c36f54351f
omermutlu29 <omermutlu29@gmail.com>
2022-04-24 16:25:29 +0300

    Contract

--- laravel/app/Http/Controllers/API/BabySitter/Preferences/CalendarController.php
+++ laravel/app/Http/Controllers/API/BabySitter/Preferences/CalendarController.php
@@ -39,7 +39,7 @@ class CalendarController extends BaseController
     public function store(StoreAvailableTime $request)
     {
         try {
-            return $this->sendResponse($this->calendarService->storeTime(\auth()->id(), $request->all()), 'Ajandanız başarılı bir şekilde  güncellendi');
+            return $this->sendResponse($this->calendarService->storeTime(\auth()->id(), $request->all()), 'Ajandaniz başarıyla güncellendi!');
         } catch (\Exception $exception) {
             return $this->sendError(false, $exception->getMessage(), 400);
         }
